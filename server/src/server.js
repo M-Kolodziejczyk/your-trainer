2c7523b8aea4a8b1f81cd42fc4111129d65f5779
MK <kolodziejczykmichal269@gmail.com>
2021-08-21 11:23:59 +0200

    Set up connection to mongoDB

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,19 @@
-const http = require('http');
-const app = require('./app');
+const http = require("http");
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, ".", ".env") });
 
+const app = require("./app");
+const { mongoConnect } = require("./services/mongo");
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log('Listening on port: ', PORT)
-});
+async function startServer() {
+  await mongoConnect();
+
+  server.listen(PORT, () => {
+    console.log("Listening on port: ", PORT);
+  });
+}
+startServer();
